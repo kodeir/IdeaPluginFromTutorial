28cd145234d386aebddb00ace55d36e3cfe09280
Kodeir <kodeir@gmail.com>
2017-03-10 14:44:22 +0300

    constructor defined icon is added

--- src/TextBoxes.java
+++ src/TextBoxes.java
@@ -1,10 +1,19 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.util.IconLoader;
 
 /**
  * Created by Sergei "Kodeir" Riabinin on 10/03/17.
  */
 public class TextBoxes extends AnAction{
+
+    public TextBoxes(){
+        // defining menu item name
+        super("Text __Boxxes",
+                "dummy description",
+                IconLoader.getIcon("/crash_test_dummy.png"));
+    }
+
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
 
