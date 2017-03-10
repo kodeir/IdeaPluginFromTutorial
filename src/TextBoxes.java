9575b1af4598284774247124dcc9caf978f87cfc
Kodeir <kodeir@gmail.com>
2017-03-10 14:34:24 +0300

    Class created - http://www.jetbrains.org/intellij/sdk/docs/basics/getting_started/creating_an_action.html

---
+++ src/TextBoxes.java
@@ -0,0 +1,12 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by Sergei "Kodeir" Riabinin on 10/03/17.
+ */
+public class TextBoxes extends AnAction{
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+
+    }
+}
